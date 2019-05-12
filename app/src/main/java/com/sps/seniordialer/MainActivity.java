0b10d2e1397f68c38c7b3eddb370c9cc54201d91
Saihajpreet Singh <44710980+saihaj@users.noreply.github.com>
2019-05-11 19:57:21 -0500

    Fixed Delete button

--- app/src/main/java/com/sps/seniordialer/MainActivity.java
+++ app/src/main/java/com/sps/seniordialer/MainActivity.java
@@ -155,6 +155,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onDelete(v);
+            }
+        });
+
 
     }
 
