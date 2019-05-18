4421aa64eb318629182870bcea6f9b7b6580cbee
Saihajpreet Singh <44710980+saihaj@users.noreply.github.com>
2019-05-17 19:36:48 -0500

    Implemented Full Screen Mode with sticky
    Signed-off-by: Saihajpreet Singh <44710980+saihaj@users.noreply.github.com>

--- app/src/main/java/com/sps/seniordialer/MainActivity.java
+++ app/src/main/java/com/sps/seniordialer/MainActivity.java
@@ -17,7 +17,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import static android.content.Intent.ACTION_CALL;
+import static android.view.View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -69,6 +70,32 @@ public class MainActivity extends AppCompatActivity {
                 onDelete(v);
             }
         });
+    }
+
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSystemUI();
+        }
+    }
+
+    private void hideSystemUI() {
+
+        /** Enables regular immersive mode.
+        For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
+        Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        View decorView = getWindow().getDecorView(); */
+
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        /** Set the content to appear under the system bars so that the
+                         content doesn't resize when the system bars hide and show. */
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 
      //Listner for buttons
