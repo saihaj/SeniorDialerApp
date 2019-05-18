a2755b5c6e5f718c510104854c865b251135b65c
Saihajpreet Singh <44710980+saihaj@users.noreply.github.com>
2019-05-17 19:38:26 -0500

    Fixed error in full screen mode
    Signed-off-by: Saihajpreet Singh <44710980+saihaj@users.noreply.github.com>

--- app/src/main/java/com/sps/seniordialer/MainActivity.java
+++ app/src/main/java/com/sps/seniordialer/MainActivity.java
@@ -81,14 +81,14 @@ public class MainActivity extends AppCompatActivity {
 
     private void hideSystemUI() {
 
-        /** Enables regular immersive mode.
+        /* Enables regular immersive mode.
         For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
-        Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-        View decorView = getWindow().getDecorView(); */
+        Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY */
 
+        View decorView = getWindow().getDecorView();
         decorView.setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                        /** Set the content to appear under the system bars so that the
+                        /* Set the content to appear under the system bars so that the
                          content doesn't resize when the system bars hide and show. */
                         | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
