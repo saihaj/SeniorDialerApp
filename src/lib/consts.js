f1be6168074a24cac8e968dcecb3ae899eaf2889
saihaj <44710980+saihaj@users.noreply.github.com>
2020-07-18 15:10:38 -0500

    feat: dialer screen

---
+++ src/lib/consts.js
@@ -0,0 +1,44 @@
+const BUTTON_VALS = {
+  1: 1,
+  2: 2,
+  3: 3,
+  4: 4,
+  5: 5,
+  6: 6,
+  7: 7,
+  8: 8,
+  9: 9,
+  0: 0,
+  star: '*',
+  hash: '#',
+}
+
+export const BUTTONS_EN = [
+  { name: '1', value: BUTTON_VALS[ 1 ] },
+  { name: '2', value: BUTTON_VALS[ 2 ] },
+  { name: '3', value: BUTTON_VALS[ 3 ] },
+  { name: '4', value: BUTTON_VALS[ 4 ] },
+  { name: '5', value: BUTTON_VALS[ 5 ] },
+  { name: '6', value: BUTTON_VALS[ 6 ] },
+  { name: '7', value: BUTTON_VALS[ 7 ] },
+  { name: '8', value: BUTTON_VALS[ 8 ] },
+  { name: '9', value: BUTTON_VALS[ 9 ] },
+  { name: '*', value: BUTTON_VALS.star },
+  { name: '0', value: BUTTON_VALS[ 0 ] },
+  { name: '#', value: BUTTON_VALS.hash },
+]
+
+export const BUTTONS_PA = [
+  { name: '੧', value: BUTTON_VALS[ 1 ] },
+  { name: '੨', value: BUTTON_VALS[ 2 ] },
+  { name: '੩', value: BUTTON_VALS[ 3 ] },
+  { name: '੪', value: BUTTON_VALS[ 4 ] },
+  { name: '੫', value: BUTTON_VALS[ 5 ] },
+  { name: '੬', value: BUTTON_VALS[ 6 ] },
+  { name: '੭', value: BUTTON_VALS[ 7 ] },
+  { name: '੮', value: BUTTON_VALS[ 8 ] },
+  { name: '੯', value: BUTTON_VALS[ 9 ] },
+  { name: '*', value: BUTTON_VALS.star },
+  { name: '੦', value: BUTTON_VALS[ 0 ] },
+  { name: '#', value: BUTTON_VALS.hash },
+]
--- src/screens/Dialer.js
+++ src/screens/Dialer.js
@@ -1,18 +1,57 @@
 import React from 'react'
-import { Text, StyleSheet } from 'react-native'
+import { FlatList, StyleSheet, View, StatusBar } from 'react-native'
+import { shape, string, oneOfType, number } from 'prop-types'
+
+import Layout from '../components/Layout'
+import NumPadButton from '../components/NumPad'
+import DialButton from '../components/DialButton'
+import DialerInput from '../components/DialerInput'
+
+import { BUTTONS_EN } from '../lib/consts'
 
 const styles = StyleSheet.create( {
-  main: {
-    paddingVertical: 100,
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    fontWeight: '800',
-    fontSize: 50,
+  container: {
+    flex: 1,
+    marginTop: 8,
+    marginBottom: 5,
+    marginHorizontal: 5,
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
   },
-
 } )
-const Dialer = () => (
-  <Text style={styles.main}>Dialer Screen</Text>
-)
 
-export default Dialer
+const DialerScreen = () => {
+  const renderButton = ( { item: { name, value } } ) => <NumPadButton value={value} name={name} />
+
+  renderButton.propTypes = {
+    item: shape( {
+      name: string.isRequired,
+      value: oneOfType( [ number, string ] ).isRequired,
+    } ),
+  }
+
+  renderButton.defaultProps = {
+    item: undefined,
+  }
+
+  return (
+    <Layout>
+      <StatusBar hidden />
+      <View style={styles.container}>
+        <DialerInput />
+        <View style={{ flex: 6 }}>
+          <FlatList
+            data={BUTTONS_EN}
+            renderItem={renderButton}
+            keyExtractor={item => item.name}
+            numColumns={3}
+            scrollEnabled={false}
+          />
+        </View>
+        <DialButton />
+      </View>
+    </Layout>
+  )
+}
+
+export default DialerScreen
